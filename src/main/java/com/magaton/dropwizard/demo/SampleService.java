a6f6a889b8d6ab376e0beae20dd414f6bdcaa62c
magaton <milan.agatonovic@gmail.com>
2013-06-28 14:30:22 +0200

    cleanup

--- src/main/java/com/magaton/dropwizard/demo/SampleService.java
+++ src/main/java/com/magaton/dropwizard/demo/SampleService.java
@@ -2,6 +2,7 @@ package com.magaton.dropwizard.demo;
 
 import com.magaton.dropwizard.demo.resource.HelloWorldResource;
 import com.yammer.dropwizard.Service;
+import com.yammer.dropwizard.assets.AssetsBundle;
 import com.yammer.dropwizard.config.Bootstrap;
 import com.yammer.dropwizard.config.Environment;
 
@@ -17,6 +18,7 @@ public class SampleService extends Service<SampleConfiguration> {
 
     @Override
     public void initialize(Bootstrap<SampleConfiguration> sampleConfigurationBootstrap) {
+        sampleConfigurationBootstrap.addBundle(new AssetsBundle("/favicon.ico"));
     }
 
     @Override
